3d194d05a6e0cacff0cb67adbd72b65913d1e801
Fernando <fernando.bbosco@gmail.com>
2022-09-09 11:13:37 -0300

    aula5-lacos

---
+++ aula5/README.md
@@ -0,0 +1,24 @@
+# exercícios
+
+
+1. escreva um loop while com usando como condição uma variável número ser menor que o número 15, use qualquer nome para a variável.
+Dentro do while faça uma soma de 2 em 2 e também faça o console.log do valor da variável e um frase poética.
+
+
+2. escreva um for que execute um código 20x. Nesse código, faça 2 cálculos matemáticos e faça o console.log do resultado desses dois cálculos.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula5/na-aula.js
@@ -0,0 +1,222 @@
+// let var1 = 0
+
+// while(true) {
+//     console.log('o valor de var1 é', var1)
+//     console.log('estou no console 😎')
+//     var1++
+//     if(var1 > 3) { 
+//         break
+//     }
+// }
+
+// console.log('estou triste e sem graça')
+
+// console.log('acabou o loop')
+// var1 = 3
+
+
+// // // esse é um laço de repetição while
+// // ele executará o bloco de código infinitamente 
+// // // (para parar a executação no seu terminal, precione CTRl+C)
+
+
+
+// // while(true) {
+    
+// // }
+
+
+
+
+
+// const notasDaProva = [{nome: 'prova 1', nota: 2}]
+
+// const ListaDeCompra = [ 
+//     { produto: 'lasanha', calorias: 5000}, 
+//     { produto: 'banana', calorias: 200}, 
+//     { produto: 'chocolate', calorias: 600}, 
+//     { produto: 'chocolate', calorias: 600}, 
+//     { produto: 'chocolate', calorias: 600}, 
+//     { produto: 'chocolate', calorias: 600}, 
+//     { produto: 'chocolate', calorias: 600}, 
+//     { produto: 'chocolate', calorias: 600}, 
+// ]
+
+// const somaDeCalorias = ListaDeCompra[0].calorias + ListaDeCompra[1].calorias
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // ----------------------------------------------------
+// 2. escreva um for que execute um código 20x. 
+// Nesse código, faça 2 cálculos matemáticos e faça o console.log 
+// do resultado desses dois cálculos.
+
+
+for(let i = 0 ; i<20 ; i++ ) {
+    
+}
+
+
+
+
+
+// // // let var1 = 0;
+
+// // // while(var1 < 3) {
+// // //     console.log('o valor de var1 é', var1)
+// // //     var1 ++
+// // // }
+
+
+
+
+
+
+
+
+// // // --------------------------------------------------
+
+// // // podemos adicionar uma condição com break para interromper o laço
+
+
+
+
+// // // let var1 = 0
+// // // while(true) {
+// // //     console.log(`o valor de var1 é ${var1}`)
+// // //     if(var1 > 4) {
+// // //         break;
+// // //     }
+// // //     var1 ++
+// // // }
+
+
+
+
+
+
+// // // temos também o laço => do while
+// // // o bloco de código do do é sempre executado ao menos uma vez
+
+
+// // // let x = 4
+
+// // // do {
+// // //     console.log('o valor de x é', x, 'dentro do do')
+// // // } while (x < 4)
+
+
+// // // while(x <4 ) {
+// // //     console.log('o valor de x é', x, 'dentro do while')
+// // // }
+
+
+
+
+
+// // // ----------------------------------------------
+
+
+
+
+
+
+
+
+
+// // // let x = 0
+
+// // // do {
+// // //     console.log('aqui é o "do"')
+// // //     console.log('o valor de x é', x)
+// // //     x++
+// // // } while (x < 4) {
+// // //     console.log('aqui é o "while"')
+// // //     console.log('o valor de x é', x)
+// // //     x++
+// // // }
+
+
+
+
+
+
+
+// // // -----------------------------------
+
+// // // laço de repetição for 
+
+
+
+
+
+
+// for(let i = 0 ; i < frutas.length  ; i++ ) {
+//     console.log('esse é o valor de i', i)
+//     console.log('minha fruta é', frutas[i])
+// }
+
+///////////////////////////////
+
+const usuario = { 
+    name: 'fernando', 
+    age: 88,
+    isAlive: true,
+    endereco: {
+        rua: '',
+        number: 12
+    }
+}
+
+
+
+
+
+
+// // // for ([inicialização]; [condição]; [expressão final]) {
+// // //      bloco de código a ser executado
+// // // }
+ 
+
+
+// // let i = 0;
+// // for (; i < 9; i++) {
+// //   console.log(i);
+// //   // more statements
+// // }
+
+
+
+
+
+
+
+
+
+
+// // // -----------------------------------
+
+
+// // // for... of
+
+
+// // let frutas = ['maça', 'banana', ]
+
+// // for(let i of frutas) {
+// //     console.log('a fruta é', i)
+// // }
+
+
+// // 
